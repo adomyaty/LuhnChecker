b3d7f91a5ba73ef8cb8ca1b5991bea4feef32767
adomyaty <a.domyaty@live.com>
2018-11-06 14:55:29 -0800

    Made output more readable. Fixed formating

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
 import LuhnCheck
 
-result = (LuhnCheck.is_luhn_valid(6011472730641911))
-print ("Correct:" + str(result))
+result = (LuhnCheck.is_luhn_valid(5378875165993419))
+print ("Passed Luhn Check? [" + str(result) + "]")
